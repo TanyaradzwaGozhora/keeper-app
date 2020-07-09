ff593ebd3e05cb9da6cd6d34ddd822168f115b74
Tanya <tanyaradzwagozhora@gmail.com>
2020-07-09 14:17:41 +0200

    Updates

---
+++ precache-manifest.cfefa88205b8dc564e3536881d7be744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b265c5e1b0be526e59cd34f09ad727dc",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "f8a507d3e81a0fa6904c",
+    "url": "/keeper-app/static/js/2.bc508148.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/keeper-app/static/js/2.bc508148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c413127d1df4459d942e",
+    "url": "/keeper-app/static/js/main.53a9fabd.chunk.js"
+  },
+  {
+    "revision": "9efce025e1cc176d2de9",
+    "url": "/keeper-app/static/js/runtime-main.d410b0b7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.90baf55cc6cc0acbc17be8a27f140712.js"
+  "/keeper-app/precache-manifest.cfefa88205b8dc564e3536881d7be744.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/keeper-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
